1c004f78259f68683fa09e1955f84687bca80019
Daniyal Samat <daniyal.samat@narxoz.kz>
2024-06-14 14:53:18 +0500

    Added cyls and conditions

--- js/script.js
+++ js/script.js
@@ -8,13 +8,29 @@ const personalMovieDB = {
     privat: false
 }
 
-const f1 = prompt("Один из последних просмотренных фильмов?", ''),
-      r1 = prompt("На сколько оцените его?", ''),
-      f2 = prompt("Один из последних просмотренных фильмов?", ''),
-      r2 = prompt("На сколько оцените его?", '');
 
-personalMovieDB.movies[f1] = r1;
-personalMovieDB.movies[f2] = r2;
+for (let i = 0; i < 2; i++) {
+    const f1 = prompt("Один из последних просмотренных фильмов?", ''),
+          r1 = prompt("На сколько оцените его?", '');
+    
+    if (f1 != null && r1 != null && f1 != '' && r1 != '' && f1.length < 50) {
+        personalMovieDB.movies[f1] = r1;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Error");
+}
 
 console.log(personalMovieDB.count);
 console.log(personalMovieDB.movies);
